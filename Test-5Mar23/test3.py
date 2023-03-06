d6e237b2c9b584bf791ff1fcfe10a3c71965ad94
deepkumarchaudhary <39412586+deepkumarchaudhary@users.noreply.github.com>
2023-03-06 20:50:56 +0530

    Update test3.py

--- Test-5Mar23/test3.py
+++ Test-5Mar23/test3.py
@@ -1,3 +1,18 @@
+"""
+https://stackoverflow.com/questions/72172274/python-pothole-fixing-machine
+Problem statement
+
+There are a similar versions of this task, but I haven't found answer to this particular one. Basically, there are two roads L1 and L2. The have the same length. The road can consist of a pothole ('X') and a normal concrete ('.'). Your task is to find the most optimal way to cross the road from left to right while finding the maximum number of potholes that can be fixed.
+
+Consider the example:
+
+L1 = '.xxx...x'
+
+L2 = '..x.xxxx'
+
+In this scenario the maximum number of potholes we can fix is 6. 
+"""
+
 def solution(L1, L2 ):
     count = 0
     #if 'x' in [L2[0], L1[0]]:
